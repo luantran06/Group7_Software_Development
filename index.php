3dfcc5a6555fd6300a2189a4fd0e4bc32a301045
unknown <rayadeb101@gmail.com>
2024-07-22 18:42:50 -0400

    updating

--- index.php
+++ index.php
@@ -383,98 +383,128 @@ if (isset($_SESSION['email'])) {
 
 
   <!-- 
-  - #FOOTER
--->
+    - #FOOTER
+  -->
 
-<footer class="footer">
-  <div class="container">
-    <div class="footer-top section">
-      <div class="footer-brand">
-        <a href="#" class="logo">
-          <img src="./picture/logo.svg" width="129" height="40" alt="Blogy logo">
-        </a>
-        <p class="footer-text">
-          The best review website for the greater Atlanta area.
+  <footer class="footer">
+    <div class="container">
+
+      <div class="footer-top section">
+
+        <div class="footer-brand">
+
+          <a href="#" class="logo">
+            <img src="./picture/logo.svg" width="129" height="40" alt="Blogy logo">
+          </a>
+
+          <p class="footer-text">
+            The best review website for the greater Atlanta area.
+          </p>
+
+        </div>
+
+        <ul class="footer-list">
+
+          <li>
+            <p class="h5">Social</p>
+          </li>
+
+          <li class="footer-list-item">
+            <ion-icon name="logo-facebook"></ion-icon>
+
+            <a href="#" class="footer-link hover:underline">Facebook</a>
+          </li>
+
+          <li class="footer-list-item">
+            <ion-icon name="logo-twitter"></ion-icon>
+
+            <a href="#" class="footer-link hover:underline">Twitter</a>
+          </li>
+
+          <li class="footer-list-item">
+            <ion-icon name="logo-pinterest"></ion-icon>
+
+            <a href="#" class="footer-link hover:underline">Pinterest</a>
+          </li>
+
+          <li class="footer-list-item">
+            <ion-icon name="logo-vimeo"></ion-icon>
+
+            <a href="#" class="footer-link hover:underline">Vimeo</a>
+          </li>
+
+        </ul>
+
+        <ul class="footer-list">
+
+          <li>
+            <p class="h5">About</p>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Style Guide</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Features</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Contact</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">404</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Privacy Policy</a>
+          </li>
+
+        </ul>
+
+        <ul class="footer-list">
+
+          <li>
+            <p class="h5">Features</p>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Upcoming Events</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Blog & News</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Features</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">FAQ Question</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link hover:underline">Testimonial</a>
+          </li>
+
+        </ul>
+
+        
+
+      </div>
+
+      <div class="section footer-bottom">
+
+        <p class="copyright">
+          &copy; Team 7 2024. Published by <a href="#" class="copyright-link hover:underline">Team 7</a>.
         </p>
+
       </div>
 
-      <!-- Wrapper for the three sections -->
-      <div class="footer-sections-wrapper">
-        <div class="footer-sections">
-          <ul class="footer-list">
-            <li>
-              <p class="h5">Social</p>
-            </li>
-            <li class="footer-list-item">
-              <ion-icon name="logo-facebook"></ion-icon>
-              <a href="#" class="footer-link hover:underline">Facebook</a>
-            </li>
-            <li class="footer-list-item">
-              <ion-icon name="logo-twitter"></ion-icon>
-              <a href="#" class="footer-link hover:underline">Twitter</a>
-            </li>
-            <li class="footer-list-item">
-              <ion-icon name="logo-pinterest"></ion-icon>
-              <a href="#" class="footer-link hover:underline">Pinterest</a>
-            </li>
-            <li class="footer-list-item">
-              <ion-icon name="logo-vimeo"></ion-icon>
-              <a href="#" class="footer-link hover:underline">Vimeo</a>
-            </li>
-          </ul>
-
-          <ul class="footer-list">
-            <li>
-              <p class="h5">About</p>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Style Guide</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Features</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Contact</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">404</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Privacy Policy</a>
-            </li>
-          </ul>
-
-          <ul class="footer-list">
-            <li>
-              <p class="h5">Features</p>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Upcoming Events</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Blog & News</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Features</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">FAQ Question</a>
-            </li>
-            <li>
-              <a href="#" class="footer-link hover:underline">Testimonial</a>
-            </li>
-          </ul>
-        </div>
-
-        <!-- "Claim this business" link -->
-        <div class="claim-link-container">
-          <a href="#" class="claim-business-link">Claim this business</a>
-        </div>
-      </div>
     </div>
-  </div>
-</footer>
-
+  </footer>
 
 
 
