2ce84146c3403ef60939f38f200ddf81215b1a4a
unknown <rayadeb101@gmail.com>
2024-07-22 18:32:18 -0400

    updating to add restaurant_id

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2024 at 08:01 PM
+-- Generation Time: Jul 23, 2024 at 12:31 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -37,15 +37,19 @@ CREATE TABLE `users` (
   `password` varchar(200) NOT NULL,
   `role` varchar(20) NOT NULL DEFAULT 'client',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `is_admin` tinyint(1) DEFAULT 0
+  `is_admin` tinyint(1) DEFAULT 0,
+  `restaurant_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `address`, `password`, `role`, `created_at`, `is_admin`) VALUES
-(3, 'Admin', 'Account', 'email@gmail.com', '5554446666', 'address', '$2y$10$H0k0IrDVJSVcg8SjJ/F6q.MD0gwsPGPqDx0tZ83LLlD9ocl1EDtU2', 'client', '2024-07-16 02:24:00', 1);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `address`, `password`, `role`, `created_at`, `is_admin`, `restaurant_id`) VALUES
+(3, 'Admin', 'Account', 'email@gmail.com', '5554446666', 'address', '$2y$10$H0k0IrDVJSVcg8SjJ/F6q.MD0gwsPGPqDx0tZ83LLlD9ocl1EDtU2', 'client', '2024-07-16 02:24:00', 1, NULL),
+(4, 'Jane', 'Smith', 'janesmith@email.com', '5555555555', 'Jane Smith Address', '$2y$10$xTdzcAFzbrTljXQzllWTGObwQQEE/SKzbLPimEIpa5G2aYwnEvRhi', 'client', '2024-07-22 06:39:44', 0, NULL),
+(5, 'John', 'Smith', 'johnsmith@email.com', '5555555554', 'johnsmith', '$2y$10$7aU36J14go03GZNWl5T.iOLLxmWHZ0HtTAB5qcYylWS.PqVMrAmg6', 'client', '2024-07-22 07:01:55', 0, NULL),
+(6, 'Restaurant A', '', 'restaurantA@gmail.com', '5555555555', 'Restaurant A, Atlanta', '$2y$10$62bT7/o8EFyUjlWNFa32GePW1SxA7R/U29wilWfV.IM78lidGEiuC', 'business', '2024-07-22 21:55:07', 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -56,7 +60,8 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `address
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `restaurant_id` (`restaurant_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -66,7 +71,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_restaurant` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant_info`.`restaurants` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
